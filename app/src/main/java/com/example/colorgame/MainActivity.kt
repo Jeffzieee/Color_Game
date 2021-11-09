87fc38fc0917b6140b4b0a7f65d20d21e8624ee9
Jephin John <jephinjohn85@gmail.com>
2021-11-09 12:10:29 +0530

    New Color Features Added.

--- app/src/main/java/com/example/colorgame/MainActivity.kt
+++ app/src/main/java/com/example/colorgame/MainActivity.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
+ //Jeffzieee
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
